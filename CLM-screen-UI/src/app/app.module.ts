714f2e708f9d3fb55454d129a8f73e9ce080a089
akshay12111 <aksdes121@gmail.com>
2022-07-05 13:21:43 +0530

    setup-componets

--- CLM-screen-UI/src/app/app.module.ts
+++ CLM-screen-UI/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
